1eb46e0515006902db0f6142286dba0867037cad
Alex <alexpleonhardt@gmail.com>
2024-07-17 14:25:36 -0400

    homepage skeleton

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,24 @@
 import styles from "./page.module.css";
+import Image from "next/image";
 
 export default function HomePage() {
   return (
-    <main className='main'>
-      
+    <main className={`main ${styles.main}`}>
+      <section className={`${styles.intro}`}>
+        
+      </section>
+      <section className={`${styles.techstack}`}>
+
+      </section>
+      <section className={`${styles.projects}`}>
+
+      </section>
+      <section className={`${styles.blogposts}`}>
+
+      </section>
+      <section className={`${styles.contact}`}>
+
+      </section>
     </main>
   );
 }
