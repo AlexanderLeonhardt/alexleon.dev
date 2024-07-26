4ef635624cece0d756b97f9bc22a4ebeb0a94fae
Alex <alexpleonhardt@gmail.com>
2024-07-26 15:23:02 -0400

    project page initialized

---
+++ src/app/projects/page.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.css';
+
+export default function ProjectsPage() {
+  return (
+    <main className={`main card ${styles.main}`}>
+      <h1>Projects</h1>
+    </main>
+  );
+}
