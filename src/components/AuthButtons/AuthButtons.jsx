c08386dfdfb0c35e292f96bb50e1c4ef53673ad3
Alex <alexpleonhardt@gmail.com>
2024-08-08 14:12:53 -0400

    fixed indentation

--- src/components/AuthButtons/AuthButtons.jsx
+++ src/components/AuthButtons/AuthButtons.jsx
@@ -3,25 +3,25 @@ import { signIn, signOut } from "@/auth"
 export function SignInButton() {
   return (
     <form
-        action={async () => {
-            "use server";
-            await signIn("github", {redirectTo: "/dashboard"});
-        }}
+      action={async () => {
+        "use server";
+        await signIn("github", {redirectTo: "/dashboard"});
+      }}
     >
-        <button type="submit">Signin with GitHub</button>
+      <button type="submit">Signin with GitHub</button>
     </form>
   );
 }
 
 export function SignOutButton() {
-    return (
+  return (
     <form
-        action={async () => {
-            "use server"
-            await signOut();
-        }}
+      action={async () => {
+        "use server"
+        await signOut();
+      }}
     >
-        <button type="submit">Sign Out</button>
+      <button type="submit">Sign Out</button>
     </form>
-    );
+  );
 }
