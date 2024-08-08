89f36c0b41694f927a931ba55c04c599c453d982
Alex <alexpleonhardt@gmail.com>
2024-08-08 12:37:12 -0400

    signin button

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,9 +1,11 @@
 import styles from './page.module.css';
+import { SignInButton } from '@/components/SignInButton/SignInButton';
 
 function DashboardPage() {
   return (
     <main>
       <h1>Dashboard</h1>
+      <SignInButton />
     </main>
   );
 }
