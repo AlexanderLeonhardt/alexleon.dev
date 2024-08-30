8806c2827b15d3d2f707661f5af1741840f35174
Alex <alexpleonhardt@gmail.com>
2024-08-30 17:32:42 -0400

    init tiptap floating menu extension

--- src/components/TipTap/TipTap.jsx
+++ src/components/TipTap/TipTap.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { BubbleMenu, useEditor, EditorContent } from '@tiptap/react'
+import { FloatingMenu, BubbleMenu, useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 
 const Tiptap = () => {
@@ -35,6 +35,28 @@ const Tiptap = () => {
           </button>
         </div>
       </BubbleMenu>}
+      {editor && <FloatingMenu editor={editor} tippyOptions={{ duration: 100 }}>
+        <div className="floating-menu">
+          <button
+            onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+            className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+          >
+            H1
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+            className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+          >
+            H2
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleBulletList().run()}
+            className={editor.isActive('bulletList') ? 'is-active' : ''}
+          >
+            Bullet list
+          </button>
+        </div>
+      </FloatingMenu>}
       <EditorContent editor={editor} />
     </>
   );
