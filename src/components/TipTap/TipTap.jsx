beafb9d0c71b8e6d2a3562505a0c58d45c3de88d
Alex <alexpleonhardt@gmail.com>
2024-08-30 17:22:28 -0400

    init tiptap bubble menu extension

--- src/components/TipTap/TipTap.jsx
+++ src/components/TipTap/TipTap.jsx
@@ -1,15 +1,43 @@
 'use client'
 
-import { useEditor, EditorContent } from '@tiptap/react'
+import { BubbleMenu, useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 
 const Tiptap = () => {
   const editor = useEditor({
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit
+    ],
     content: '<p>Hello World! 🌎️</p>',
   })
 
-  return <EditorContent editor={editor} />
+  return (
+    <>
+      {editor && <BubbleMenu editor={editor} tippyOptions={{ duration: 100 }}>
+        <div className="bubble-menu">
+          <button
+            onClick={() => editor.chain().focus().toggleBold().run()}
+            className={editor.isActive('bold') ? 'is-active' : ''}
+          >
+            Bold
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleItalic().run()}
+            className={editor.isActive('italic') ? 'is-active' : ''}
+          >
+            Italic
+          </button>
+          <button
+            onClick={() => editor.chain().focus().toggleStrike().run()}
+            className={editor.isActive('strike') ? 'is-active' : ''}
+          >
+            Strike
+          </button>
+        </div>
+      </BubbleMenu>}
+      <EditorContent editor={editor} />
+    </>
+  );
 }
 
 export default Tiptap
