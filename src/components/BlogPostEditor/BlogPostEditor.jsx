8cfde62a9cd3da2d1dc33ab96b9ba3fe44b21c21
Alex <alexpleonhardt@gmail.com>
2024-09-12 17:17:53 -0400

    dash blog styling

--- src/components/BlogPostEditor/BlogPostEditor.jsx
+++ src/components/BlogPostEditor/BlogPostEditor.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+import styles from './styles.module.css';
 import { useState } from 'react';
 import TipTap from '@/components/TipTap/TipTap';
 import { blogPostSubmit, deleteBlogPost, updateBlogPost } from '@/serveractions/blogPostActions';
@@ -33,17 +34,21 @@ export default function BlogPostEditor({ blogPosts }) {
     <div>
       {!editingPost ? (
         <>
-          <button onClick={handleCreateNew}>Create New Post</button>
-          <div className="blogPostList">
+          <button className={styles.create} onClick={handleCreateNew}><p>Create Post</p><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M13.0001 10.9999L22.0002 10.9997L22.0002 12.9997L13.0001 12.9999L13.0001 21.9998L11.0001 21.9998L11.0001 12.9999L2.00004 13.0001L2 11.0001L11.0001 10.9999L11 2.00025L13 2.00024L13.0001 10.9999Z"></path></svg></button>
+          <div className={styles.list}>
             {blogPosts.map((blogPost) => (
-              <div key={blogPost.slug} className="blogPost">
-                <div>
+              <div key={blogPost.slug} className={styles.blogPost}>
+                <div className={styles.content}>
                   <p><a href={`/blog/${blogPost.slug}`}>{blogPost.title}</a></p>
-                  <p>{new Date(blogPost.createdAt).toLocaleDateString("en-US", { month: 'long', day: 'numeric', year: 'numeric' })}</p>
+                  <p className={styles.date}>{new Date(blogPost.createdAt).toLocaleDateString("en-US", { month: 'long', day: 'numeric', year: 'numeric' })}</p>
                 </div>
-                <div className="buttons">
-                  <button onClick={() => deleteBlogPost(blogPost.slug)}>Delete</button>
-                  <button onClick={() => handleEditClick(blogPost)}>Edit</button>
+                <div className={styles.buttons}>
+                  <button onClick={() => handleEditClick(blogPost)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M12.8995 6.85453L17.1421 11.0972L7.24264 20.9967H3V16.754L12.8995 6.85453ZM14.3137 5.44032L16.435 3.319C16.8256 2.92848 17.4587 2.92848 17.8492 3.319L20.6777 6.14743C21.0682 6.53795 21.0682 7.17112 20.6777 7.56164L18.5563 9.68296L14.3137 5.44032Z"></path></svg>
+                  </button>
+                  <button className={styles.delete} onClick={() => deleteBlogPost(blogPost.slug)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor"><path d="M17 6H22V8H20V21C20 21.5523 19.5523 22 19 22H5C4.44772 22 4 21.5523 4 21V8H2V6H7V3C7 2.44772 7.44772 2 8 2H16C16.5523 2 17 2.44772 17 3V6ZM9 11V17H11V11H9ZM13 11V17H15V11H13ZM9 4V6H15V4H9Z"></path></svg>
+                  </button>
                 </div>
               </div>
             ))}
--- src/components/TipTap/TipTap.jsx
+++ src/components/TipTap/TipTap.jsx
@@ -114,9 +114,9 @@ const Tiptap = ({ postTitle='', postDescription='', postContent, onSave, onCance
         </div>
         <EditorContent editor={editor} />
       </div>
-      <div>
-        <button onClick={handleSaveClick}>Save</button>
-        <button onClick={onCancel}>Cancel</button>
+      <div className={styles.buttons}>
+        <button className={styles.save} onClick={handleSaveClick}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M18 21V13H6V21H4C3.44772 21 3 20.5523 3 20V4C3 3.44772 3.44772 3 4 3H17L21 7V20C21 20.5523 20.5523 21 20 21H18ZM16 21H8V15H16V21Z"></path></svg></button>
+        <button className={styles.cancel} onClick={onCancel}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z"></path></svg></button>
       </div>
     </>
   );
