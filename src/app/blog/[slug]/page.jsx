4294b66e21be8935915dcdda4200773dcaaca399
Alex <alexpleonhardt@gmail.com>
2024-09-13 11:12:29 -0400

    replaced description with creation date

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -15,15 +15,21 @@ export async function generateMetadata({ params }) {
 export default async function BlogPostPage({ params }) {
   const { slug } = params;
   const blogPost = await getBlogPost(slug);
-  
   if (!blogPost) notFound();
-  const markup = { __html: blogPost.content};
+
+  const options = {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+  }
+  const dateString = blogPost.createdAt.toLocaleDateString("en-US", options);
+  const content = { __html: blogPost.content};
 
   return (
     <main className={`main card`}>
       <h1>{blogPost.title}</h1>
-      <p>{blogPost.description}</p>
-      <div dangerouslySetInnerHTML={markup} />
+      <p>{dateString}</p>
+      <div dangerouslySetInnerHTML={content} />
     </main>
   );
 }
