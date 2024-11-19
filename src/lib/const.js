71a58567f2f859e3bff6a06fc2d97d15b2b3bf7d
Alex <alexpleonhardt@gmail.com>
2024-11-19 17:44:56 -0500

    const tech

---
+++ src/lib/const.js
@@ -0,0 +1,46 @@
+import { HTML5Icon, CSS3Icon, JavaScriptIcon, MongoDBIcon, ExpressIcon, ReactIcon, NodeJSIcon, NextJSIcon, TypeScriptIcon, TailwindIcon } from '@/components/Icons/Icons';
+
+const tech = {
+  'html5': {
+    'icon': HTML5Icon,
+    'name': "HTML",
+  },
+  'css3': {
+    'icon': CSS3Icon,
+    'name': "CSS",
+  },
+  'javascript': {
+    'icon': JavaScriptIcon,
+    'name': "JavaScript",
+  },
+  'mongodb': {
+    'icon': MongoDBIcon,
+    'name': "MongoDB",
+  },
+  'express': {
+    'icon': ExpressIcon,
+    'name': "Express",
+  },
+  'react': {
+    'icon': ReactIcon,
+    'name': "React",
+  },
+  'nodejs': {
+    'icon': NodeJSIcon,
+    'name': "Node.js",
+  },
+  'nextjs': {
+    'icon': NextJSIcon,
+    'name': "Next.js",
+  },
+  'typescript': {
+    'icon': TypeScriptIcon,
+    'name': "TypeScript",
+  },
+  'tailwind': {
+    'icon': TailwindIcon,
+    'name': "Tailwind",
+  },
+}
+
+export { tech }
